512e9e27cd84f96795b66a0b40750ab02cea0130
brbursey <blainebursey@yahoo.com>
2020-08-15 01:22:32 -0400

    Start creating Node class 1. install Math.Net library for linear algebra 2. Create Node class with Linear and activation functions 3. unit tests

--- NNApp/Program.cs
+++ NNApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using MathNet.Numerics.LinearAlgebra;
 using NeuralNetworks;
 
 namespace NNApp
@@ -7,8 +10,13 @@ namespace NNApp
     {
         static void Main(string[] args)
         {
-            var test = new Test();
-            Console.WriteLine(test.Hello());
+            var m = Matrix<double>.Build.Random(500, 500);
+            var v = MathNet.Numerics.LinearAlgebra.Vector<double>.Build.Random(500);
+            var y = m.Solve(v);
+            Console.WriteLine(y);
+            Console.WriteLine(m);
+
+            var tensor = new Vector4();
         }
     }
 }
